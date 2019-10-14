8cf654496db28e76884876520b90be739b3c6598
hiroshi urainkyo <biz.hr.urainkyo@gmail.com>
2019-10-14 13:31:41 +0900

    add logger and cors and graceful timeout.

--- go.mod
+++ go.mod
@@ -2,5 +2,9 @@ module github.com/purini-to/gqlgen-todos
 
 require (
 	github.com/99designs/gqlgen v0.10.1
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/purini-to/zapmw v1.1.0 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/vektah/gqlparser v1.1.2
+	go.uber.org/multierr v1.2.0 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/purini-to/zapmw"
+	"github.com/rs/cors"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/99designs/gqlgen/handler"
 	gqlgen_todos "github.com/purini-to/gqlgen-todos"
 )
 
 const defaultPort = "8080"
+const defaultGracefulTimeout = time.Second * 60
+
+func withRequestID(logger *zap.Logger, r *http.Request) *zap.Logger {
+	reqID := middleware.GetReqID(r.Context())
+	if len(reqID) == 0 {
+		return logger
+	}
+
+	return logger.With(zap.String("reqId", reqID))
+}
+
+func corsMiddleware() func(http.Handler) http.Handler {
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8080"},
+		AllowCredentials: true,
+	})
+
+	return c.Handler
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,9 +46,44 @@ func main() {
 		port = defaultPort
 	}
 
-	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(gqlgen_todos.NewExecutableSchema(gqlgen_todos.Config{Resolvers: &gqlgen_todos.Resolver{}})))
+	logger, _ := zap.NewDevelopment()
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	r := chi.NewRouter()
+
+	r.Use(
+		middleware.RequestID,
+		zapmw.WithZap(logger, withRequestID), // logger with request id.
+		zapmw.Request(zapcore.InfoLevel, "request"),
+		corsMiddleware(),
+	)
+
+	r.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	r.Handle("/query", handler.GraphQL(gqlgen_todos.NewExecutableSchema(gqlgen_todos.Config{Resolvers: &gqlgen_todos.Resolver{}})))
+
+	s := http.Server{Addr: fmt.Sprintf(":%s", port), Handler: r}
+
+	go func() {
+		logger.Info(fmt.Sprintf("Connect to http://localhost:%s/ for GraphQL playground", port))
+		logger.Info("Listen and serve", zap.String("transport", "HTTP"), zap.String("port", port))
+		if err := s.ListenAndServe(); err != http.ErrServerClosed {
+			logger.Fatal("Listen failed", zap.Error(err))
+		}
+	}()
+
+	sig := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		sig <- fmt.Errorf("%s", <-c)
+	}()
+
+	logger.Info(fmt.Sprintf("SIGNAL %v received, then shutting down...", <-sig), zap.Duration("timeout", defaultGracefulTimeout))
+	ctx, cancel := context.WithTimeout(context.Background(), defaultGracefulTimeout)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		// Error from closing listeners, or context timeout:
+		logger.Error("Failed to gracefully shutdown", zap.Error(err))
+	}
+
+	logger.Info("Exit")
 }
